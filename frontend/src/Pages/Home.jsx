4f242cec9a9b3e4993fe6954b200eb6447415b4f
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2025-02-25 00:03:02 +0530

    feature: add hero section 	new file:   frontend/src/assets/LANDING.mp4 	new file:   frontend/src/components/Hero.jsx

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
     <>
       <Navbar fontColor="text-white" />
-      {/* hero section with image and text  */}
-      <section className="flex flex-col items-center justify-center h-screen bg-hero-pattern bg-cover bg-center bg-no-repeat bg-amber-500">
-        <h1 className="text-4xl font-bold text-white">
-          Welcome to our website
-        </h1>
-        <p className="text-white">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </p>
-      </section>
+      <Hero />
     </>
   );
 };
