dd6c77f3d943912b063432d0fc1ad071be1df28c
Deneth  Godakandagei <138301396+DenethGodakandagei@users.noreply.github.com>
2025-02-26 14:53:57 +0530

    Carousel Animation

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -18,7 +18,7 @@ const slides = [
   {
     title: "GALLE",
     description:
-      "Galle is a historic coastal city in southern Sri Lanka, known for its well-preserved colonial architecture, stunning beaches, and vibrant culture. It is most famous for the Galle Fort, a UNESCO World Heritage Site built by the Portuguese in the 16th century and later expanded by the Dutch. The fort features cobblestone streets, charming cafés, boutique shops, and beautiful ocean views.",
+      "Galle is a historic coastal city in southern Sri Lanka, known for its well-preserved colonial architecture, stunning beaches, and vibrant culture. It is most famous for the Galle Fort, a UNESCO World Heritage Site built by the Portuguese in the 16th century and later expanded by the Dutch. The fort features and beautiful ocean views.",
     background:
       "https://images.unsplash.com/photo-1589373797397-d19670f47549?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
@@ -43,7 +43,7 @@ export default function Carousel() {
 
   // Auto-play
   useEffect(() => {
-    const interval = setInterval(nextSlide, 4000);
+    const interval = setInterval(nextSlide, 6000);
     return () => clearInterval(interval);
   }, [nextSlide]);
 
@@ -55,79 +55,90 @@ export default function Carousel() {
           key={slides[currentIndex].title}
           className="absolute inset-0 w-full h-full bg-cover bg-center"
           style={{ backgroundImage: `url(${slides[currentIndex].background})` }}
-          initial={{ opacity: 0.5 }}
+          initial={{ opacity: 0.7 }}
           animate={{ opacity: 1 }}
-          exit={{ opacity: 0.5 }}
-          transition={{ duration: 0.2 }}
+          exit={{ opacity: 0.7 }}
+          transition={{ duration: 0.1 }}
         >
           <div className="absolute inset-0 bg-black/20"></div>
         </motion.div>
       </AnimatePresence>
 
       {/* Slide Content */}
-      
       {slides.map((slide, index) => (
-  <div
-    key={index}
-    className={`absolute inset-0 flex top-1/3 left-20 w-2/5 text-white p-8 transition-all duration-1000 ease-in-out transform ${
-      currentIndex === index
-        ? "opacity-100 -translate-y-10 scale-100 " 
-        : "opacity-0 translate-y-20 scale-8d0 "
-    }`}
-  >
-    <div className="max-w-3xl">
-      <h1 className="text-5xl font-bold">{slide.title}</h1>
-      <p className="mt-4 text-lg">{slide.description}</p>
-      <button className="bg-white m-2 p-3 text-black rounded-xl transition-transform duration-300 transform hover:scale-105">
-        SEE MORE SITES
-      </button>
-    </div>
-  </div>
-))}
-
-
-
-
-
-
-
+        <AnimatePresence mode="wait" key={index}>
+          {currentIndex === index && (
+            <div className="absolute inset-0 flex md:top-1/3 md:left-20 md:w-2/5 md:m-0 m-10 text-white ">
+              <div className="md:max-w-3xl overflow-hidden">
+                <motion.h1
+                  key={index}
+                  initial={{ opacity: 0, y: 40, scale: 1 }}
+                  animate={{ opacity: 1, y: 0, scale: 1 }}
+                  exit={{ opacity: 1, y: -60, scale: 1 }}
+                  transition={{ duration: 0.7, ease: "easeInOut" }}
+                  className="md:text-5xl text-3xl font-bold overflow-hidden"
+                >
+                  {slide.title}
+                </motion.h1>
+                <div className="overflow-hidden mt-10 mb-1">
+                  <motion.h1
+                    key={index}
+                    initial={{ opacity: 0, y: 120, scale: 1 }}
+                    animate={{ opacity: 1, y: 0, scale: 1 }}
+                    exit={{ opacity: 0, y: -80, scale: 1 }}
+                    transition={{ duration: 0.5, ease: "easeIn", exit: { duration: 0.2, ease: "easeIn" } }}
+                    className="overflow-hidden"
+                  >
+                    <p className="md:text-lg text-lg">{slide.description}</p>
+                  </motion.h1>
+                </div>
+                <div className="   flex inset-0  justify-self-start items-center  md:mt-16">
+        <button className="bg-white text-black md:mt-0 mt-4 py-3 px-6 rounded-md md:text-xl text-sm shadow-lg transform transition-all duration-300 hover:scale-110 hover:shadow-2xl focus:outline-none">
+          SEE MORE SITES
+        </button>
+      </div>
+              </div>
+             
+            </div>
+          )}
+        </AnimatePresence>
+      ))}
+     
       {/* Slide Indicators */}
-      <div className="absolute top-1/2 left-10 transform -translate-y-1/2 flex flex-col items-center gap-2">
-        <div className="w-[2px] h-12 bg-white/50 p-0" />
+      <div className="absolute top-1/2 left-10 transform -translate-y-1/2  flex-col items-center hidden lg:block">
+        <div className="w-[2px] h-12 bg-white/50 ml-[11px]" />
         {slides.map((_, index) => (
           <div key={index} className="flex flex-col items-center">
             <div
-              className={`m-1 rounded-full text-center ${
-                index === currentIndex
+              className={` rounded-full text-center ${index === currentIndex
                   ? "bg-white/80 w-6 h-6"
                   : "bg-white w-3 h-3"
-              }`}
+                }`}
             />
             {index !== slides.length - 1 && (
               <div className="w-[2px] h-12 bg-white/50 p-0" />
             )}
           </div>
         ))}
-        <div className="w-[2px] h-12 bg-white/50 p-0" />
+        <div className="w-[2px] h-12 bg-white/50 ml-[11px]" />
       </div>
 
       {/* Image slider */}
-      <div className="absolute bottom-10 left-1/2 flex top-1/2 items-center transform -translate-y-1/2 gap-4 justify-start">
-        {slides.map((slide, index) => {
-          return (
-            <img
-              key={index}
-              src={slide.background}
-              alt={slide.title}
-              className={`object-cover rounded-2xl transition-transform duration-700 ease-in-out transform cursor-pointer ${
-                index === currentIndex
-                  ? "w-[260px] h-[340px] rotate-y-0 scale-110 shadow-xl opacity-100"
-                  : "w-[220px] h-[240px]  hover:opacity-100 hover:scale-105 rotate-y-15"
+
+      <div className="absolute md:left-3/4 left-2/4 flex md:top-2/6 bottom-20  mr-10 transform items-center">
+        {slides.map((slide, index) => (
+          <img
+            key={index}
+            src={slide.background}
+            alt={slide.title}
+            className={`object-cover rounded-2xl transition-transform -left-4/4 duration-700 ease-in-out transform cursor-pointer 
+        ${index === currentIndex
+                ? "lg:w-[290px] lg:h-[380px] md:w-[240px]  md:h-[250px] w-[140px] h-[160px] scale-110 shadow-xl opacity-100 absolute"
+                : "lg:w-[220px] lg:h-[240px] md:w-[210px] md:h-[200px] w-[70px] h-[70px] mx-3"
               }`}
-              onClick={() => setCurrentIndex(index)}
-            />
-          );
-        })}
+            onClick={() => setCurrentIndex(index)}
+          />
+        ))}
       </div>
     </div>
   );
