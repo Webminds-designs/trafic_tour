770f36315e1104553c2e826d81c79f1396752bba
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2025-02-26 21:55:05 +0530

    fature/ home package category

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import HomePackageCat from "../components/HomePackageCat";
 
 const Home = () => {
   return (
     <>
       <Navbar fontColor="text-white" />
       <Hero />
+      <HomePackageCat />
     </>
   );
 };
---
+++ frontend/src/components/HomePackageCat.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const HomePackageCat = () => {
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center p-24">
+      <div className="w-full h-96 bg-white flex justify-between">
+        <p className="flex text-xl w-full md:w-1/2 lg:w-[500px] text-black items-center">
+          STEP INTO A LAND WHERE ANCIENT HERITAGE MEETS BREATHTAKING LANDSCAPES.
+          FROM MISTY MOUNTAINS TO SUN-KISSED BEACHES, EVERY DESTINATION TELLS A
+          STORY. IMMERSE YOURSELF IN A JOURNEY OF CULTURE, ADVENTURE, AND
+          UNFORGETTABLE MEMORIES, CAREFULLY CURATED TO MAKE YOUR VISIT TRULY
+          EXTRAORDINARY.
+        </p>
+        <div className="flex text-8xl w-full md:w-1/2 lg:w-[700px] text-black items-center">
+          UNVEIL DESTINATIONS BEYOND DREAMS
+        </div>
+      </div>
+      <div className="w-full h-96 bg-amber-600"></div>
+    </div>
+  );
+};
+
+export default HomePackageCat;
