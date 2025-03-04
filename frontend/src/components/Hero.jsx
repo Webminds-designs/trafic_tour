5d26a1da0b8baa7df495eb2cb0db5a5a575ba48d
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2025-03-04 11:09:21 +0530

    modified:  frontend/index.html -  add fonts - code next and figtree figtree is default font modified:  frontend/src/components/Hero.jsx -  used fonts - code next and figtree 	new file:   frontend/src/assets/fonts/CodeNext-Trial-Regular.ttf 	modified:   frontend/src/components/Hero.jsx 	modified:   frontend/src/index.css

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import video from "../assets/LANDING.mp4";
 
 const Hero = () => {
   return (
-    <section className="flex flex-col items-start justify-center h-screen bg-hero-pattern bg-cover bg-center bg-no-repeat bg-black">
+    <section className="flex flex-col items-start justify-end h-screen bg-hero-pattern bg-cover bg-center bg-no-repeat bg-black">
       <video
         className="w-full h-screen object-cover opacity-70"
         src={video}
@@ -11,17 +11,20 @@ const Hero = () => {
         loop
         muted
       ></video>
-      <div className="absolute h-2/3 flex flex-col items-start justify-center z-10 text-start w-full md:w-1/2 lg:w-[600px] px-4 md:px-24 gap-16">
-        <h1 className=" text-5xl md:text-6xl font-bold  text-white">
-          A WIBDOW TO NEW ADVANTURES
-        </h1>
-        <p className="text-white text-xl md:text-2xl">
-          THE PATH TO DISCOVERY IS LIMITLESS, OFFERING VIEWS OF LANDSCAPES YET
-          TO BE EXPLORED. EVERY JOURNEY STARTS WITH CURIOSITY, AND TRAFFIC TOURS
-          IS HERE TO TURN THAT CURIOSITY INTO UNFORGETTABLE EXPERIENCES.
+      <div className="absolute h-2/3 flex flex-col items-start justify-end z-10 text-start w-full md:w-2/3 lg:w-[800px] px-4 md:px-12 gap-8 py-8">
+        <div>
+          <h1 className="w-2/3 text-4xl md:text-4xl  font-bold  text-white font-CodeNext-regular">
+            A winbow to New Adventure
+          </h1>
+          <hr className="w-1/3 border-white" />
+        </div>
+        <p className="text-white text-sm md:text-lg font-CodeNext-regular">
+          The path to discovery is limitless, ofering views of landscapes yet to
+          be explored. Every journey starts with curiosity, and TRAFIC TOURS is
+          here to turn that curiosity into unforgettable experiences.
         </p>
-        <div className="px-4 py-2 bg-white rounded-sm hover:bg-black hover:text-white cursor-pointer transform transition duration-300">
-          START YOUR ADVENTURE
+        <div className="px-4 py-2 text-sm bg-white rounded-3xl font-bold hover:bg-black hover:text-white cursor-pointer transform transition duration-300">
+          Start your advanture
         </div>
       </div>
     </section>
