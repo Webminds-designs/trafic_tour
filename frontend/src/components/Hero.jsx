d4f67242033240d17a1ed28c6f5288aebaacdeb2
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2025-03-05 09:12:17 +0530

    modify: add margin to bottm of the button

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -14,14 +14,15 @@ const Hero = () => {
 
       <div className="absolute font-codeNext h-2/3 flex flex-col items-start justify-end z-10 text-start w-full md:w-1/2 lg:w-[600px] px-4 md:px-24 gap-8">
         <h1 className=" text-5xl md:text-5xl font-bold  text-white">
-        A window to new <span className="border-b-1">adventures</span>
+          A window to new <span className="border-b-1">adventures</span>
         </h1>
         <p className="text-white text-sm md:text-sm">
-        The path to discovery is limitless, offering views of landscapes yet to be explored. Every journey starts with curiosity, and Traffic Tours is here to turn that curiosity into unforgettable experiences.
+          The path to discovery is limitless, offering views of landscapes yet
+          to be explored. Every journey starts with curiosity, and Traffic Tours
+          is here to turn that curiosity into unforgettable experiences.
         </p>
-        <div className="px-4 py-2 bg-white rounded-2xl hover:bg-black hover:text-white cursor-pointer transform transition duration-300">
+        <div className="px-4 py-2 bg-white rounded-2xl hover:bg-black mb-4 hover:text-white cursor-pointer transform transition duration-300">
           Start your Advanture
-
         </div>
       </div>
     </section>
